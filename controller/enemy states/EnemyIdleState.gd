3ac2a1cad7d13cb047e43dc7b3495c2f9e74e345
amiani <amianijohns@gmail.com>
2019-11-18 19:21:46 -0500

    added delay

--- controller/enemy states/EnemyIdleState.gd
+++ controller/enemy states/EnemyIdleState.gd
@@ -4,5 +4,11 @@ class_name EnemyIdleState
 func enter(controller)->void:
   controller.actor.velocity = Vector3()
 
+var delay = .2 #seconds
+var time = 0
 func update(controller, delta)->ActorState:
-  return EnemyWalkState.new()
+  .update(controller, delta)
+  time += delta
+  if time >= delay:
+    return controller.states.walk.new()
+  return null
