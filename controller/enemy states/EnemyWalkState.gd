5c4c986c572ce8f53108587ec79d0f519f6a09f8
amiani <amianijohns@gmail.com>
2019-11-18 19:22:17 -0500

    moved steering logic to own method

--- controller/enemy states/EnemyWalkState.gd
+++ controller/enemy states/EnemyWalkState.gd
@@ -11,6 +11,18 @@ func exit(controller):
   controller.actor.animation.stop()
 
 func update(controller, delta)->ActorState:
+  if hit:
+    return controller.states.hit.new()
+  var actor = controller.actor
+  steer(actor)
+  var players = actor.get_tree().get_nodes_in_group('players')
+  for p in players:
+    var distance = p.global_position - actor.global_position
+    if abs(distance.y) <= actor.hitPlaneTolerance && abs(distance.x) < 110 && abs(distance.x) > 80:
+      return controller.states.idle.new()
+  return null
+
+func steer(actor):
   var interest = []
   var danger = []
   for i in range(0, resolution):
@@ -18,8 +30,8 @@ func update(controller, delta)->ActorState:
     danger.push_back(0)
 
   for b in behaviours:
-    var behaviourInterest = b.getInterest(controller.actor, resolution)
-    var behaviourDanger = b.getDanger(controller.actor, resolution)
+    var behaviourInterest = b.getInterest(actor, resolution)
+    var behaviourDanger = b.getDanger(actor, resolution)
     for i in range(0, resolution):
       interest[i] = max(interest[i], behaviourInterest[i])
       danger[i] = max(danger[i], behaviourDanger[i])
@@ -34,9 +46,8 @@ func update(controller, delta)->ActorState:
       magnitude = interest[i]
 
   var angle = 2*PI * direction/resolution
-  controller.actor.velocity.x = cos(angle) * controller.actor.walkSpeed
-  controller.actor.velocity.y = sin(angle) * controller.actor.walkSpeed
-  return null
+  actor.velocity.x = cos(angle) * actor.walkSpeed * magnitude
+  actor.velocity.y = sin(angle) * actor.walkSpeed * magnitude
 
 func maskInterest(interest:Array, danger:Array)->Array:
   for i in range(0, interest.size()):
