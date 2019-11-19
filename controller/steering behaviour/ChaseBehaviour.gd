6e6117010c70808b2bdb8eb9145a3aa3038c3765
amiani <amianijohns@gmail.com>
2019-11-18 19:24:34 -0500

    calculate angle between actors with position

--- controller/steering behaviour/ChaseBehaviour.gd
+++ controller/steering behaviour/ChaseBehaviour.gd
@@ -4,11 +4,10 @@ class_name ChaseBehaviour
 func getInterest(actor:Actor, resolution:int)->Array:
   var interest = initSteeringMap(resolution)
   var players = actor.get_tree().get_nodes_in_group('players')
-  #breakpoint
   for p in players:
-    var angle = actor.get_angle_to(p.global_position)
+    var angle = actor.get_angle_to(p.position)
     var interestDirection = floor(resolution * angle / (2*PI))
-    interest[interestDirection] = .5
+    interest[interestDirection] = 1
   return interest
 
 func getDanger(actor:Actor, resolution:int):
