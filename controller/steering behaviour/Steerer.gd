feb588562dd8e09d1741ca88791f138bf10a4609
amiani <amianijohns@gmail.com>
2019-11-18 19:22:26 -0500

    removed steerer

--- controller/steering behaviour/Steerer.gd
+++
@@ -1,8 +0,0 @@
-extends Reference
-class_name Steerer
-
-const behaviours = []
-var resolution : int
-func _init(behaviours:Array, resolution:int):
-  self.behaviours = behaviours
-  self.resolution = resolution
