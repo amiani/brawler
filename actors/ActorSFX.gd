08b9d9b435d613106fe224ef1bccbf2be4ed9a0d
amiani <amianijohns@gmail.com>
2019-11-21 22:03:12 -0500

    showed Josh

--- actors/ActorSFX.gd
+++ actors/ActorSFX.gd
@@ -4,7 +4,9 @@ class_name ActorSFX
 export(AudioStreamSample) var hitStream
 func handleHit():
   bus = 'Hit Bus'
+  """
   var lowPass = AudioServer.get_bus_effect(1, 0)
-  lowPass.cutoff_hz = 
+  lowPass.cutoff_hz = randi()%500
+  """
   stream = hitStream
   play()
