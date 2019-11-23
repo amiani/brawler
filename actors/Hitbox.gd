471dd7a2e6ef103e040264516708909476c5d8d3
amiani <amianijohns@gmail.com>
2019-11-23 10:56:51 -0500

    added hurtbox check

--- actors/Hitbox.gd
+++ actors/Hitbox.gd
@@ -3,3 +3,6 @@ class_name HitBox
 
 export var damage = 0
 var actor
+
+func _ready():
+  get_child(0).disabled = true
--- controller/StateController.gd
+++ controller/StateController.gd
@@ -6,7 +6,6 @@ export(NodePath) var actorPath
 var actor
 func _ready() -> void:
   actor = get_node(actorPath)
-  #actor.find_node('HurtBox').connect('area_entered', self, 'handleHurtboxEntered')
   actor.animation.connect('animation_finished', self, 'handleAnimationFinished')
 
 func updateState(delta):
@@ -16,12 +15,19 @@ func updateState(delta):
     state = nextState
     state.enter(self)
 
-signal hit
+signal hurt
 func handleHurtboxEntered(area:Area2D):
   if area.is_in_group('hitboxes') && area.actor != actor:
     var yDistance = abs(area.actor.position.y - actor.position.y)
     if yDistance < actor.hitPlaneTolerance:
       state.handleHurt(self, area)
+      emit_signal('hurt')
+
+signal hit
+func handleHitboxEntered(area:Area2D):
+  if area.is_in_group('hurtboxes') && area.actor != actor:
+    var yDistance = abs(area.actor.position.y - actor.position.y)
+    if yDistance < actor.hitPlaneTolerance:
       emit_signal('hit')
 
 func handleAnimationFinished(animation):
