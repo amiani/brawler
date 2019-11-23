ad6e4eb4eb09ddbce1b334274eb8b8eebbeac4b8
amiani <amianijohns@gmail.com>
2019-11-23 11:48:09 -0500

    attackState only checking last 10 frames

--- controller/player states/PlayerAttackState.gd
+++ controller/player states/PlayerAttackState.gd
@@ -11,10 +11,11 @@ func enter(controller)->void:
   actor.animation.play(data.animation)
   actor.hitbox.damage = data.damage
 
+var inputTolerance = 10
 func update(controller, delta) -> ActorState:
   if animationFinished:
     var inputs = controller.inputs
-    for i in range(controller.inputs.size()-1, -1, -1):
+    for i in range(controller.inputs.size() - inputTolerance - 1, -1, -1):
       for c in data.combo:
         if c.relevanceMask & controller.inputs[i].mask == c.inputMask:
           var nextAttack = controller.attacks[c.attack]
